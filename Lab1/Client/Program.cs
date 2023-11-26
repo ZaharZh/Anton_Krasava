d00610e60b0ca3492b5354fecf653394db803004
Zahar Zhirkov <93308838+ZaharZh@users.noreply.github.com>
2023-11-26 22:08:05 +0500

    Add files via upload

---
+++ Lab1/Client/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+class PipeClient
+{
+    public struct an
+    {
+        public int x;
+        public int y;
+        public int z;
+    }
+    static void Main(string[] args)
+    {
+        var ton = new NamedPipeClientStream(".", "pashcha", PipeDirection.InOut);
+        
+        Console.WriteLine("Спроба падлучэння да трубы. ..");
+        ton.Connect();
+        
+        Console.WriteLine("Падлучаны да трубы.");
+        
+        byte[] bates = new byte[Unsafe.SizeOf<an>()];
+        ton.Read(bates);
+        var m = MemoryMarshal.Read<an>(bates);
+
+        m.x -= 1;
+        m.y -= 2;
+        m.z -= 3;
+
+        Console.WriteLine($"Вось Iкс: {m.x}, вось Iгрэк: {m.y}, вось Зэт: {m.z}");
+        
+        byte[] bytes = new byte[Unsafe.SizeOf<an>()];
+        MemoryMarshal.TryWrite<an>(bytes, m);
+        ton.Write(bytes);
+           
+        Console.WriteLine("Нацiснiце Enter, каб працягнуць...");  
+    }
+}
---
+++ Lab1/Server/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+class PipeServer
+{
+    public struct an
+    {
+        public int x;
+        public int y;
+        public int z;
+    }
+    static void Main()
+    {
+        var an1 = new an {x=7, y=8, z=9};
+        
+        Console.WriteLine("Створаны аб'ект ПатокCервераНайменнагаКанала.");
+        
+        var ton = new NamedPipeServerStream("pashcha", PipeDirection.InOut);
+        
+        Console.WriteLine("Чаканне падлучэння клiента. ..");
+        
+        ton.WaitForConnection();
+        
+        Console.WriteLine("Клiент падлучаны.");
+        
+        byte[] bytes = new byte[Unsafe.SizeOf<an>()];
+        MemoryMarshal.TryWrite<an>(bytes, an1);
+        ton.Write(bytes);
+        
+        Console.WriteLine($"Вось Iкс: {an1.x}, вось Iгрэк: {an1.y}, вось Зэт: {an1.z}");
+
+        byte[] bates = new byte[Unsafe.SizeOf<an>()];
+        ton.Read(bates);
+        MemoryMarshal.Read<an>(bates); 
+
+    }
+}
